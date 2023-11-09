60d3de55c4634fdc43d040d808a8f51aa8947954
Eduardo Antonio <edusilvamld@gmail.com>
2023-11-09 13:56:35 +0000

    teste

--- cursoLogicaProg/script.js
+++ cursoLogicaProg/script.js
@@ -1,5 +1,5 @@
-const numeros = [1, 2, 3, 4, 5];
+const numeros = [1, 2, 3, 4, 5]
 
-for(let i = 0; i < numeros.length; i++) {
-  console.log(numeros[i]);
+for (let i = 0; i < numeros.length; i++) {
+    console.log(numeros[i])
 }
